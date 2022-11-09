d31d8c7de94c234623c6d6e8b27d885816967b0c
MD-Baki <bakimd77@gmail.com>
2022-11-10 03:03:18 +0600

    jwt add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -23,6 +24,14 @@ async function run() {
         const reviewCollection = client
             .db("photographer")
             .collection("reviews");
+
+        app.post("/jwt", (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "1h",
+            });
+            res.send({ token });
+        });
 
         app.post("/service", async (req, res) => {
             const service = req.body;
